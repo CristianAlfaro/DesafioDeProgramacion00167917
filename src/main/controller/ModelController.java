634765448811852354753a75eb484669fb9d5ee8
CristianAlfaro <00167917@uca.edu.sv>
2020-07-01 11:19:58 -0600

    interfaz 2.0

--- src/main/controller/ModelController.java
+++ src/main/controller/ModelController.java
@@ -6,6 +6,7 @@ import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -30,12 +31,39 @@ public class ModelController implements Initializable {
     double y = 0;
 
     @FXML private Pane pane1;
-    @FXML private Pane pane2;
     @FXML private Pane pane3;
-    @FXML private Polygon up1;
-    @FXML private Polygon up2;
     @FXML private Button btn_mesh;
     @FXML private ImageView imgMesh;
+    @FXML private Button up;
+    @FXML private Button down;
+    @FXML private Button right;
+    @FXML private Button left;
+    @FXML private Button nodos;
+
+    @FXML private Label info2;
+    @FXML private Label info3;
+    @FXML private ImageView info1;
+    @FXML private Label info4;
+    @FXML private ImageView info5;
+    @FXML private ImageView info6;
+    @FXML private ImageView info7;
+    @FXML private ImageView info8;
+
+    int i = 0;
+    int j = 7;
+
+    Boolean flags = false;
+
+    String [][] modelo = {{"18.png","17.png","16.png","15.png","14.png","13.png","12.png","11.png"},
+            {"28.png","27.png","26.png","25.png","24.png","23.png","22.png","21.png"}};
+
+    String [][] mallado = {{"38.png","37.png","36.png","35.png","34.png","33.png","32.png","31.png"},
+            {"48.png","47.png","46.png","45.png","44.png","43.png","42.png","41.png"}};
+
+    String [][] nodes = {{"58.png","57.png","56.png","55.png","54.png","53.png","52.png","51.png"},
+            {"68.png","67.png","66.png","65.png","64.png","63.png","62.png","61.png"}};
+
+
 
     @FXML
     public void home (MouseEvent event) {
@@ -63,6 +91,7 @@ public class ModelController implements Initializable {
         stage.setX(event.getScreenX() - x);
         stage.setY(event.getScreenY() - y);
     }
+
     @FXML
     void pressed (MouseEvent event) {
         x = event.getSceneX();
@@ -72,19 +101,12 @@ public class ModelController implements Initializable {
     @FXML
     public void pane1 (MouseEvent event){
         pane1.setVisible(true);
-        up1.setVisible(true);
-        pane2.setVisible(false);
-        up2.setVisible(false);
         pane3.setVisible(false);
     }
 
     @FXML
-    public void pane2 (MouseEvent event){
-        pane1.setVisible(false);
-        up1.setVisible(false);
-        pane2.setVisible(true);
-        up2.setVisible(true);
-        pane3.setVisible(false);
+    public void condiciones (MouseEvent event){
+        condiciones();
     }
 
     @FXML
@@ -97,18 +119,122 @@ public class ModelController implements Initializable {
         pane3.setVisible(false);
     }
 
-    @FXML
-    public void mesh (MouseEvent event){
-        if(btn_mesh.getText().equals("Malla")){
-            btn_mesh.setText("Dominio");
-            Image image = new Image("main/resources/fondo.jpg");
+    public void condiciones(){
+        if(up.isVisible()){
+            String img = "main/resources/boundary.png";
+            Image image = new Image(img);
             imgMesh.setImage(image);
-        } else if(btn_mesh.getText().equals("Dominio")){
-            btn_mesh.setText("Malla");
-            Image image = new Image("main/resources/hestia_family.jpg");
-            imgMesh.setImage(image);
+            controler(false);
+        } else{
+            controler(true);
+            setImage();
         }
 
+    }
+    
+    
+    public void setImage () {
+        if (btn_mesh.getText().equals("Malla")) {
+            String img = "main/resources/" + modelo[i][j];
+            Image image = new Image(img);
+            imgMesh.setImage(image);
+        } else if (btn_mesh.getText().equals("Dominio")) {
+            String img;
+            if(flags){
+                img = "main/resources/" + nodes[i][j];
+            }else{
+                img = "main/resources/" + mallado[i][j];
+            }
+            Image image = new Image(img);
+            imgMesh.setImage(image);
+        }
+    }
+
+    @FXML
+    public void up (MouseEvent event){
+        if(i == 1){
+            i-=1;
+        }else {
+            i+=1;
+        }
+        setImage();
+    }
+
+    @FXML
+    public void down (MouseEvent event){
+        if(i == 0){
+            i+=1;
+        }else {
+            i-=1;
+        }
+        setImage();
+
+    }
+
+    @FXML
+    public void right (MouseEvent event){
+        if(j == 7){
+            j-=7;
+        }else {
+            j+=1;
+        }
+        setImage();
+    }
+
+    @FXML
+    public void left (MouseEvent event){
+        if(j == 0){
+            j+=7;
+        }else {
+            j-=1;
+        }
+        setImage();
+    }
+
+    @FXML
+    public void mesh (MouseEvent event){
+        meshAux();
+    }
+
+    @FXML
+    public void nod (MouseEvent event){
+        if(flags){
+            flags = false;
+            setImage();
+        } else {
+            flags = true;
+            setImage();
+        }
+    }
+
+
+    public void meshAux(){
+        controler(true);
+        if(btn_mesh.getText().equals("Malla")){
+            btn_mesh.setText("Dominio");
+            setImage();
+            nodos.setVisible(true);
+        } else if(btn_mesh.getText().equals("Dominio")){
+            btn_mesh.setText("Malla");
+            nodos.setVisible(false);
+            setImage();
+        }
+    }
+    
+    
+    public void controler(Boolean flag){
+        up.setVisible(flag);
+        down.setVisible(flag);
+        right.setVisible(flag);
+        left.setVisible(flag);
+        info1.setVisible(flag);
+        info2.setVisible(flag);
+        info3.setVisible(flag);
+        info4.setVisible(!flag);
+        info5.setVisible(!flag);
+        info6.setVisible(!flag);
+        info7.setVisible(!flag);
+        info8.setVisible(!flag);
     }
 
 
--- src/main/controller/Step1Controller.java
+++ src/main/controller/Step1Controller.java
@@ -3,16 +3,24 @@ package main.controller;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import main.mainApp;
 import main.scenes.FactoryScene;
 import main.scenes.TypeScene;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static jdk.nashorn.internal.parser.TokenType.CASE;
 
 public class Step1Controller implements Initializable {
 
@@ -28,6 +36,17 @@ public class Step1Controller implements Initializable {
     @FXML private Pane pane7;
     @FXML private Pane pane8;
     @FXML private Pane pane9;
+
+    @FXML private Button step1;
+    @FXML private Button step2;
+    @FXML private Button step3;
+    @FXML private Button step4;
+    @FXML private Button step5;
+    @FXML private Button step6;
+    @FXML private Button componentes;
+    @FXML private Button ensamblaje;
+    @FXML private Button step8;
+    @FXML private Button home;
 
     @FXML
     public void back (MouseEvent event) {
@@ -58,124 +77,183 @@ public class Step1Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        focused(step1);
     }
 
     @FXML
-    public void pane1 (MouseEvent event){
+    public void pass1 (MouseEvent event){
+        passAux(1);
+    }
+
+    @FXML
+    public void pass2 (MouseEvent event){
+        passAux(2);
+    }
+
+    @FXML
+    public void pass3 (MouseEvent event){
+        passAux(3);
+    }
+
+    @FXML
+    public void pass4 (MouseEvent event){
+        passAux(4);
+    }
+
+    @FXML
+    public void pass5 (MouseEvent event){
+        passAux(5);
+    }
+
+    @FXML
+    public void pass6 (MouseEvent event){
+        passAux(6);
+    }
+
+    @FXML
+    public void pass7 (MouseEvent event){
+        passAux(7);
+    }
+
+    @FXML
+    public void pass8 (MouseEvent event){
+        passAux(8);
+    }
+
+    @FXML
+    public void pass9 (MouseEvent event){
+        passAux(9);
+    }
+
+    public void passAux (int pass){
+        switch (pass){
+            case 1:
+                step2.setDisable(false);
+                pane2();
+                break;
+            case 2:
+                step3.setDisable(false);
+                pane3();
+                break;
+            case 3:
+                step4.setDisable(false);
+                pane4();
+                break;
+            case 4:
+                step5.setDisable(false);
+                pane5();
+                break;
+            case 5:
+                step6.setDisable(false);
+                pane6();
+                break;
+            case 6:
+                componentes.setDisable(false);
+                pane7();
+                break;
+            case 7:
+                ensamblaje.setDisable(false);
+                pane8();
+                break;
+            case 8:
+                step8.setDisable(false);
+                pane9();
+                break;
+            case 9:
+                home.setDisable(false);
+                break;
+        }
+    }
+
+    @FXML
+    public void pane1 (){
+        paneAux();
+        focused(step1);
         pane1.setVisible(true);
-        pane2.setVisible(false);
-        pane3.setVisible(false);
-        pane4.setVisible(false);
-        pane5.setVisible(false);
-        pane6.setVisible(false);
-        pane7.setVisible(false);
-        pane8.setVisible(false);
-        pane9.setVisible(false);
     }
 
     @FXML
-    public void pane2 (MouseEvent event){
-        pane1.setVisible(false);
+    public void pane2 (){
+        paneAux();
+        focused(step2);
         pane2.setVisible(true);
-        pane3.setVisible(false);
-        pane4.setVisible(false);
-        pane5.setVisible(false);
-        pane6.setVisible(false);
-        pane7.setVisible(false);
-        pane8.setVisible(false);
-        pane9.setVisible(false);
     }
 
     @FXML
-    public void pane3 (MouseEvent event){
-        pane1.setVisible(false);
-        pane2.setVisible(false);
+    public void pane3 (){
+        paneAux();
+        focused(step3);
         pane3.setVisible(true);
-        pane4.setVisible(false);
-        pane5.setVisible(false);
-        pane6.setVisible(false);
-        pane7.setVisible(false);
-        pane8.setVisible(false);
-        pane9.setVisible(false);
     }
 
     @FXML
-    public void pane4 (MouseEvent event){
-        pane1.setVisible(false);
-        pane2.setVisible(false);
-        pane3.setVisible(false);
+    public void pane4 (){
+        paneAux();
+        focused(step4);
         pane4.setVisible(true);
-        pane5.setVisible(false);
-        pane6.setVisible(false);
-        pane7.setVisible(false);
-        pane8.setVisible(false);
-        pane9.setVisible(false);
     }
 
     @FXML
-    public void pane5 (MouseEvent event){
-        pane1.setVisible(false);
-        pane2.setVisible(false);
-        pane3.setVisible(false);
-        pane4.setVisible(false);
+    public void pane5 (){
+        paneAux();
+        focused(step5);
         pane5.setVisible(true);
-        pane6.setVisible(false);
-        pane7.setVisible(false);
-        pane8.setVisible(false);
-        pane9.setVisible(false);
     }
 
     @FXML
-    public void pane6 (MouseEvent event){
-        pane1.setVisible(false);
-        pane2.setVisible(false);
-        pane3.setVisible(false);
-        pane4.setVisible(false);
-        pane5.setVisible(false);
+    public void pane6 (){
+        paneAux();
+        focused(step6);
         pane6.setVisible(true);
-        pane7.setVisible(false);
-        pane8.setVisible(false);
-        pane9.setVisible(false);
     }
 
     @FXML
-    public void pane7 (MouseEvent event){
-        pane1.setVisible(false);
-        pane2.setVisible(false);
-        pane3.setVisible(false);
-        pane4.setVisible(false);
-        pane5.setVisible(false);
-        pane6.setVisible(false);
+    public void pane7 (){
+        paneAux();
+        focused(componentes);
         pane7.setVisible(true);
-        pane8.setVisible(false);
-        pane9.setVisible(false);
     }
 
     @FXML
-    public void pane8 (MouseEvent event){
-        pane1.setVisible(false);
-        pane2.setVisible(false);
-        pane3.setVisible(false);
-        pane4.setVisible(false);
-        pane5.setVisible(false);
-        pane6.setVisible(false);
-        pane7.setVisible(false);
+    public void pane8 (){
+        paneAux();
+        focused(ensamblaje);
         pane8.setVisible(true);
-        pane9.setVisible(false);
     }
 
     @FXML
-    public void pane9 (MouseEvent event){
-        pane1.setVisible(false);
-        pane2.setVisible(false);
-        pane3.setVisible(false);
-        pane4.setVisible(false);
-        pane5.setVisible(false);
-        pane6.setVisible(false);
-        pane7.setVisible(false);
-        pane8.setVisible(false);
+    public void pane9 (){
+        paneAux();
+        focused(step8);
         pane9.setVisible(true);
     }
 
+    public void paneAux(){
+        pane1.setVisible(false);
+        pane2.setVisible(false);
+        pane3.setVisible(false);
+        pane4.setVisible(false);
+        pane5.setVisible(false);
+        pane6.setVisible(false);
+        pane7.setVisible(false);
+        pane8.setVisible(false);
+        pane9.setVisible(false);
+        desfocused(step1);
+        desfocused(step2);
+        desfocused(step3);
+        desfocused(step4);
+        desfocused(step5);
+        desfocused(step6);
+        desfocused(componentes);
+        desfocused(ensamblaje);
+        desfocused(step8);
+    }
+
+    public void focused(Button btn){
+        btn.setStyle("-fx-background-color: #28354c;");
+    }
+
+    public void desfocused(Button btn){
+        btn.setStyle("-fx-background-color: #161d2a;");
+    }
 }
